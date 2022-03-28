06a5b3a7dc940883f35cc5d344be328579832d2c
garrigoose <gkats11@gmail.com>
2022-03-28 15:00:02 -0400

    shipping reducers, actions created

--- zevhub-frontend/src/Components/Shipping.js
+++ zevhub-frontend/src/Components/Shipping.js
@@ -10,6 +10,8 @@ const Shipping = () => {
   const [country, setCountry] = useState('');
 
   const submitHandler = (e) => {
+    e.preventDefault();
+
     console.log('submit');
   };
 
@@ -24,6 +26,39 @@ const Shipping = () => {
             value={address}
             required
             onChange={(e) => setAddress(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='city'>
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter city'
+            value={city}
+            required
+            onChange={(e) => setCity(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='postalCode'>
+          <Form.Label>Postal Code</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter postal code'
+            value={postalCode}
+            required
+            onChange={(e) => setPostalCode(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='country'>
+          <Form.Label>Country</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter country'
+            value={country}
+            required
+            onChange={(e) => setCountry(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
